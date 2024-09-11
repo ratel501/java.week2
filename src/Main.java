a4e7eed0a5837c0af01b1d877bbf4b4f5f1c58e4
김영준 <kyj7766@gmail.com>
2024-09-11 17:20:38 +0900

    처음 커밋

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+/*
+Date : 2024-09-11
+Source : Hello.java
+Author : MinQ
+Email : 2024513@daelim.ac.kr
+*/
+class Hello {
+    public static void main(String[] args) { //프로그램의 시작
+        System.out.println("Hello, World!"); //Hello, World! 출력
+    }
+}
